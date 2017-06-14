8b116d06fb96538caba1fee445f0d8e7fdf0eafa
Izzziah <isaiah@gramboblog.com>
2017-06-14 14:57:09 -0700

    Auto-generated commit

---
+++ main.bundle.js
@@ -0,0 +1,288 @@
+webpackJsonp([1,4],{
+
+/***/ 135:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(44)(false);
+// imports
+
+
+// module
+exports.push([module.i, "h1 {\r\n    text-align: center;\r\n}\r\nlabel {\r\n    text-align: center;\r\n    /*border: 1px solid black;*/\r\n    margin: 0 auto;\r\n}\r\ninput.new-todo {\r\n    padding: 6px;\r\n    display: block;\r\n    margin-left: auto;\r\n    margin-right: auto;\r\n    border: 1px solid default;\r\n    background-color: #F0F8FF;\r\n}\r\nli {\r\n    display: block;\r\n    /*border: 1px solid black;*/\r\n}\r\nul {\r\n    width: 45%;\r\n    padding: 6px;\r\n    margin-left: auto;\r\n    margin-right: auto;\r\n    /*border: 1px solid black;*/\r\n\r\n}\r\n.destroy {\r\n    color: red;\r\n    /*border: 1px solid black;*/\r\n    float: right;\r\n    cursor: pointer;\r\n}\r\n.new-todo {\r\n    width: 50%;\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 136:
+/***/ (function(module, exports) {
+
+module.exports = "<section class=\"todoapp\">\n  <header class=\"header\">\n    <h1>Todos</h1>\n    <input class=\"new-todo\" placeholder=\"What needs to be done?\" autofocus=\"\" \n    [(ngModel)]=\"newTodo.title\" (keyup.enter)=\"addTodo()\">\n  </header>\n\n  <section class=\"main\" *ngIf=\"todos.length > 0\">\n    <ul class=\"todo-list\">\n      <li *ngFor=\"let todo of todos\" [class.completed]=\"todo.complete\">\n        <div class=\"view\">\n          <input class=\"toggle\" type=\"checkbox\" \n          (click)=\"toggleTodoComplete(todo)\" [checked]=\"todo.complete\">\n          <label>{{todo.title}}</label>\n          <label class=\"destroy\" (click)=\"removeTodo(todo)\">x</label>\n          <!--<button class=\"destroy\" (click)=\"removeTodo(todo)\"></button>-->\n        </div>\n      </li>\n    </ul>\n  </section>\n\n  <footer class=\"footer\" *ngIf=\"todos.length > 0\">\n    <span class=\"todo-count\">\n      <strong>{{todos.length}}</strong> \n      {{todos.length == 1 ? 'item' : 'items'}} left</span>\n  </footer>\n</section>\n"
+
+/***/ }),
+
+/***/ 161:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(70);
+
+
+/***/ }),
+
+/***/ 69:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 69;
+
+
+/***/ }),
+
+/***/ 70:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(76);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(78);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(81);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 77:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__todo__ = __webpack_require__(80);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__todo_data_service__ = __webpack_require__(79);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var AppComponent = (function () {
+    function AppComponent(todoDataService) {
+        this.todoDataService = todoDataService;
+        this.newTodo = new __WEBPACK_IMPORTED_MODULE_1__todo__["a" /* Todo */]();
+    }
+    AppComponent.prototype.addTodo = function () {
+        this.todoDataService.addTodo(this.newTodo);
+        this.newTodo = new __WEBPACK_IMPORTED_MODULE_1__todo__["a" /* Todo */]();
+    };
+    AppComponent.prototype.toggleTodoComplete = function (todo) {
+        this.todoDataService.toggleTodoComplete(todo);
+    };
+    AppComponent.prototype.removeTodo = function (todo) {
+        this.todoDataService.deleteTodoById(todo.id);
+    };
+    Object.defineProperty(AppComponent.prototype, "todos", {
+        get: function () {
+            return this.todoDataService.getAllTodos();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_4" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__(136),
+        styles: [__webpack_require__(135)],
+        providers: [__WEBPACK_IMPORTED_MODULE_2__todo_data_service__["a" /* TodoDataService */]]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_2__todo_data_service__["a" /* TodoDataService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__todo_data_service__["a" /* TodoDataService */]) === "function" && _a || Object])
+], AppComponent);
+
+var _a;
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 78:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(20);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(74);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(75);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(77);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 79:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(5);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TodoDataService; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var TodoDataService = (function () {
+    function TodoDataService() {
+        // Placeholder for last id so we can simulate
+        // automatic incrementing of id's
+        this.lastId = 0;
+        // Placeholder for todo's
+        this.todos = [];
+    }
+    // Simulate POST /todos
+    TodoDataService.prototype.addTodo = function (todo) {
+        if (!todo.id) {
+            todo.id = ++this.lastId;
+        }
+        this.todos.push(todo);
+        return this;
+    };
+    // Simulate DELETE /todos/:id
+    TodoDataService.prototype.deleteTodoById = function (id) {
+        this.todos = this.todos
+            .filter(function (todo) { return todo.id !== id; });
+        return this;
+    };
+    // Simulate PUT /todos/:id
+    TodoDataService.prototype.updateTodoById = function (id, values) {
+        if (values === void 0) { values = {}; }
+        var todo = this.getTodoById(id);
+        if (!todo) {
+            return null;
+        }
+        Object.assign(todo, values);
+        return todo;
+    };
+    // Simulate GET /todos
+    TodoDataService.prototype.getAllTodos = function () {
+        return this.todos;
+    };
+    // Simulate GET /todos/:id
+    TodoDataService.prototype.getTodoById = function (id) {
+        return this.todos
+            .filter(function (todo) { return todo.id === id; })
+            .pop();
+    };
+    // Toggle todo complete
+    TodoDataService.prototype.toggleTodoComplete = function (todo) {
+        var updatedTodo = this.updateTodoById(todo.id, {
+            complete: !todo.complete
+        });
+        return updatedTodo;
+    };
+    return TodoDataService;
+}());
+TodoDataService = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["c" /* Injectable */])(),
+    __metadata("design:paramtypes", [])
+], TodoDataService);
+
+//# sourceMappingURL=todo-data.service.js.map
+
+/***/ }),
+
+/***/ 80:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Todo; });
+var Todo = (function () {
+    function Todo(values) {
+        if (values === void 0) { values = {}; }
+        this.title = '';
+        this.complete = false;
+        Object.assign(this, values);
+    }
+    return Todo;
+}());
+
+//# sourceMappingURL=todo.js.map
+
+/***/ }),
+
+/***/ 81:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ })
+
+},[161]);
+//# sourceMappingURL=main.bundle.js.map
